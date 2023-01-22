5f88512ef0c28f731a53552c6c91139dddd879a1
VijayGohel <vijaygohel5454@gmail.com>
2023-01-22 17:01:12 +0530

    user specific posts in profile page

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,20 +4,26 @@ import "./Feed.css"
 import {useSelector, useDispatch} from "react-redux";
 import { useEffect } from 'react';
 import { getTimelinePosts } from '../../actions/PostAction';
+import { useParams } from 'react-router-dom';
 
 const Feed = () => {
 
     const {user} = useSelector(state=>state.authReducer.authData);
-    const {posts, loading} = useSelector(state=>state.postReducer);
+    let {posts, loading} = useSelector(state=>state.postReducer);
     const dispatch = useDispatch();
+    const params = useParams();
 
     useEffect(()=>{
         dispatch(getTimelinePosts(user._id));
     },[]);
 
+    if(params.id) 
+        posts = posts.filter((post)=>post.userId==params.id);
+
     return (
         <div className="feed">
             {
+                posts.length == 0 ? <div>No posts available</div> : 
                 loading ? "Fetching posts..." :
                 posts.map((post)=>{
                     return(
