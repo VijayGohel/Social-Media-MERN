e8cb930c99520758e3df8d848417a674ac752d7c
VijayGohel <vijaygohel5454@gmail.com>
2023-01-22 22:50:40 +0530

    auth using jwt

--- src/api/UserRequest.js
+++ src/api/UserRequest.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const API = axios.create({baseURL: "http://localhost:5000"});
 
+API.interceptors.request.use((req)=> {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+    }
+    return req;
+})
+
 export const getUser = (id)=> API.get(`/user/${id}`);
 export const updateUser = (id, data)=> API.patch(`/user/${id}`, data);
 export const getAllUsers = ()=> API.get('/user/');
